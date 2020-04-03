266790c4511bb29b98a3481926bcce3af367fccd
Toro <alex@yourprivateproxy.com>
2020-04-03 18:37:04 +0300

    latest

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -1,22 +1,29 @@
 import * as yup from 'yup';
 
 export const registerValidationSchema = yup.object().shape({
-	username: yup.string().required('Username is required!'),
+	username: yup
+		.string()
+		.required('Username is required!')
+		.min(4, 'Username needs at least 4 char!'),
+
 	password: yup
 		.string()
-		.min(6, 'Password has to be longer than 6 characters!')
+		.min(6, 'Password needs at least 6 char!')
 		.required('Password is required!'),
 	contact_name: yup.string().required('Full Name is required!'),
 	email_address: yup
 		.string()
 		.required('Email is required!')
-		.email('Must be a correct email!')
+		.email('Must be a valid email!')
 });
 
 export const loginValidationSchema = yup.object().shape({
-	username: yup.string().required('Username is required!'),
+	username: yup
+		.string()
+		.required('Username is required!')
+		.min(4, 'Username needs at least 4 char!'),
 	password: yup
 		.string()
-		.min(6, 'Password has to be longer than 6 characters!')
+		.min(6, 'Password needs at least 6 char!')
 		.required('Password is required!')
 });
